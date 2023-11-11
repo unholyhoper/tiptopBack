d69eae4a4a27febe8958a0128a990b554626dd1c
eothmani.ext <eothmani.ext@diamos.com>
2023-11-11 23:00:07 +0100

    Revert "first commit"
    This reverts commit 9ee75b75b89fc43ebd6fd943a25a40564ce05123.

--- src/main/java/com/tiptop/users/repos/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tiptop.users.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tiptop.users.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-		Role findByRole(String role);
-}
--- src/main/java/com/tiptop/users/security/SecParams.java
+++
@@ -1,7 +0,0 @@
-package com.tiptop.users.security;
-
-public interface SecParams {
-	public static final long EXP_TIME = 10*24*60*60*1000;
-	public static final String SECRET = "nadhemb@yahoo.com";
-	public static final String PREFIX = "Bearer ";
-}
