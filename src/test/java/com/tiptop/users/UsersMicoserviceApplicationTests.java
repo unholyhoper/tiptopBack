9ee75b75b89fc43ebd6fd943a25a40564ce05123
eothmani.ext <eothmani.ext@diamos.com>
2023-11-11 22:56:17 +0100

    first commit

---
+++ src/test/java/com/tiptop/users/UsersMicoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiptop.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMicoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
